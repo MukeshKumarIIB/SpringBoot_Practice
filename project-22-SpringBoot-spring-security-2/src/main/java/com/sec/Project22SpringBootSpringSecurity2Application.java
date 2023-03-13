0b4e0a2c4b5474a3abe4f62c9bff9d3267650a91
MukeshKuBehera <mukeshkubehera1@gmail.com>
2023-03-13 12:39:45 +0530

    spring security 2

---
+++ project-22-SpringBoot-spring-security-2/src/main/java/com/sec/Project22SpringBootSpringSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project22SpringBootSpringSecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project22SpringBootSpringSecurity2Application.class, args);
+	}
+
+}
